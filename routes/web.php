13cd297db1b857b305a29091064b07d4b302d351
HisyamSamAm <mhisyamnajwan@gmail.com>
2024-01-04 13:31:06 +0700

    crud

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
